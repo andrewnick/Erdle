a10e1e17d46d078fc7efffef910d4d8ba56fdb85
Andrew Nicholas <andrew.d.nicholas@gmail.com>
2022-04-01 15:01:24 +1300

    feat: add erdle links

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,37 @@
-import { Button } from "ui";
-
 export default function Web() {
   return (
     <div>
-      <h1>Web</h1>
-      <Button />
+      <h1>Erdle</h1>
+      <p>Bringing all your &quot;erdle&quot; games into one place</p>
+      <ul>
+        <li>
+          <a
+            href="https://www.nytimes.com/games/wordle/index.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Wordle
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://worldle.teuteuf.fr/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Worldle
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.heardle.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Heardle
+          </a>
+        </li>
+      </ul>
     </div>
   );
 }
